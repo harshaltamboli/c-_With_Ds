623cab1a5b8589d9e0a2404fc036e3c63896f67d
harshaltamboli <harshaltamboli429@gmail.com>
2024-04-13 22:55:10 +0530

    [ADDED]: harshal this proram is traversing

---
+++ traversing in cpp.cpp
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Using nested loops to traverse the 2D array
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.println(arr[i][j]);
+            }
+        }
+    }
+}
+
